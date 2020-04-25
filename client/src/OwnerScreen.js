698e37607724ef90dca3e7a0c0715abae88954f5
Merritt Thompson <merrittmarcthompson@gmail.com>
2020-04-25 14:32:34 -0700

    Some documentation improvements.

--- client/src/OwnerScreen.js
+++ client/src/OwnerScreen.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 import { callWebApi } from './utility.js';
 
 const indentNextStyle = {
@@ -12,9 +15,7 @@ const indentNextStyle = {
 export default function OwnerScreen(props) {
   const [requests, setRequests] = useState([]);
 
-  useEffect(() => {
-    callWebApi('GET', '/api/owner/accessRequests', wrapper => setRequests(wrapper.requests));
-  }, []);
+  useEffect(() => callWebApi('GET', '/api/owner/accessRequests', wrapper => setRequests(wrapper.requests)), []);
 
   return (
     <Accordion>
@@ -26,10 +27,49 @@ export default function OwnerScreen(props) {
             </Accordion.Toggle>
           </Card.Header>
           <Accordion.Collapse eventKey={request.id}>
-            <Card.Body>Hello! I'm the body</Card.Body>
+            <Form>
+              <RequestItems requestId={request.id} />
+            </Form>
           </Accordion.Collapse>
         </Card>
       ))}
     </Accordion>
   );
 }
+
+function RequestItems(props) {
+  const [requestItems, setRequestItems] = useState([]);
+
+  useEffect(() =>
+    callWebApi('GET', `/api/owner/accessRequestItems/${props.requestId}`, wrapper => setRequestItems(wrapper.requestItems)), [props.requestId]);
+
+  return (
+    requestItems.map((requestItem, index) => (
+      <React.Fragment key={index}>
+        <Row>
+          <Col>
+            <Form.Label>Key</Form.Label>
+          </Col>
+          <Col>
+            <Form.Control type="text" placeholder={requestItem.key} />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Form.Label size="sm">Purpose</Form.Label>
+          </Col>
+          <Col>
+            <Form.Control type="text" placeholder={requestItem.purpose} />
+          </Col>
+        </Row>
+        {/*
+        <Form.Label size="sm">Current Value</Form.Label>
+        <Form.Control type="text" placeholder={requestItem.currentValue} />
+        <Form.Label size="sm">New Value</Form.Label>
+        <Form.Control type="text" placeholder={requestItem.newValue} />
+        */}
+      </React.Fragment>
+    ))
+  );
+}
+
--- server.js
+++ server.js
@@ -5,9 +5,47 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/owner/accessRequestItems', (req, res) => {
+app.get('/api/owner/accessRequestItems/:requestId', (req, res) => {
   console.log('/api/owner/accessRequestItems');
-
+  if (req.params.requestId == "2345") {
+    res.status(200);
+    res.send({
+      requestItems: [
+        {
+          requestId: 2345,
+          key: "Amazon Credit Card Number",
+          purpose: "Purchase 1 item(s) for $19.99 total",
+          doesKeyExist: 1,
+          currentValue: "4097 1279 6350 0011",
+          newValue: null,
+        }, {
+          requestId: 2345,
+          key: "Amazon Shipping Address",
+          purpose: "Print shipping label",
+          doesKeyExist: 0,
+          currentValue: null,
+          newValue: null,
+        }
+      ]
+    });
+  } else if (req.params.requestId == "2357") {
+    res.status(200);
+    res.send({
+      requestItems: [
+        {
+          requestId: 2357,
+          key: "Roomba Floorplan",
+          purpose: "Store the current floorplan created by your robot vacuum",
+          doesKeyExist: 1,
+          currentValue: "(27,34)(19,46)(109,17)",
+          newValue: "(27,35)(20,46)(110,19)",
+        }
+      ],
+    });
+  } else {
+    res.status(400);
+    res.send();
+  }
 });
 
 app.get('/api/owner/accessRequests', (req, res) => {
@@ -17,37 +55,13 @@ app.get('/api/owner/accessRequests', (req, res) => {
     requests: [
       {
         id: 2345,
-        by: "www.amazonarama.com",
+        by: "www.amazon.com",
         timeStamp: "04/20/2020 11:36:05",
       }, {
         id: 2357,
-        by: "www.irobot.com",
+        by: "irobot-Roomba-s9-000136007",
         timeStamp: "04/20/2020 13:30:17",
       },
-    ],
-    requestDataItems: [
-      {
-        requestId: 2345,
-        key: "Amazon Shipping Address",
-        purpose: "Print shipping label",
-        doesKeyExist: 0,
-        currentValue: null,
-        newValue: null,
-      }, {
-        requestId: 2345,
-        key: "Amazon Credit Card Number",
-        purpose: "Purchase 1 item(s) for $19.99 total",
-        doesKeyExist: 1,
-        currentValue: "4097 1279 6350 0011",
-        newValue: null,
-      }, {
-        requestId: 2357,
-        key: "Roomba Floorplan",
-        purpose: "Store the current floorplan created by your robot vacuum",
-        doesKeyExist: 1,
-        currentValue: "(27,34)(19,46)(109,17)",
-        newValue: "(27,35)(20,46)(110,19)",
-      }
     ],
   });
 });
